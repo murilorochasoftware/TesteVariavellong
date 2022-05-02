63fb888ceb95e8f518922c2b77e9951c9c680fe0
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:42:01 -0300

    Teste do uso de variável com o tipo de dados long em Java.

---
+++ src/TesteVariavelLong.java
@@ -0,0 +1,8 @@
+public class TesteVariavelLong {
+
+    public static void main(String[] args) {
+        long quantidadeEleitores = 2147483648L;
+
+        System.out.printf("A quantidade de eleitores é: %d", quantidadeEleitores);
+    }
+}
